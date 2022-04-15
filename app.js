c9f67030f02ff01d347f39025fc1e2b3f0a5a909
xenia iporac <xenia.iporac@gmail.com>
2022-04-15 16:02:28 +0800

    remove about collection connection

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const path = require('path');
 const ejsMate = require('ejs-mate');
 const mongoose = require('mongoose');
 const Tourground = require('./models/tourground');
-const Tourground = require('./models/about');
 const { truncate } = require("fs");
 const mongoSanitize = require('express-mongo-sanitize');
 const dbUrl = process.env.DB_URL;
